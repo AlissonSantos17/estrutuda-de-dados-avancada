55cb2284b1a1ece5a062ac0b85c0f9427a08e08e
AlissonSantos17 <alisson.wiin@gmail.com>
2021-09-13 18:18:31 -0300

    add altura

--- ArvoreBinaria.py
+++ ArvoreBinaria.py
@@ -8,14 +8,14 @@ class ArvoreBinaria:
   def inserir(self, valor):
     novo = No(valor)
     #Verificação caso a arvore esteja vazia
-    if self.raiz is None:
+    if (self.raiz is None):
       self.raiz = novo
     else:
       atual = self.raiz
       while True:
         pai = atual
         #Esquerda
-        if valor <= atual.valor:
+        if (valor <= atual.valor):
           atual = atual.esquerda
           if atual is None:
             pai.esquerda = novo
@@ -24,18 +24,46 @@ class ArvoreBinaria:
         #Direita
         else:
           atual = atual.direita
-          if atual is None:
+          if (atual is None):
             pai.direita = novo
             self.ligacoes.append(str(pai.valor) + '->' + str(novo.valor))
             return
   
   def pesquisar(self, valor):
     atual = self.raiz
-    while atual.valor != valor:
-      if valor < atual.valor:
+
+    while (atual.valor != valor):
+      if (valor < atual.valor):
         atual = atual.esquerda
       else:
         atual = atual.direita
-      if atual is None:
+      if (atual is None):
         return None
     return atual
+  
+  def getNoFolha():
+    pass
+
+  def getGrau(self, no_atual):
+    pass
+
+  def altura(self, no=None):
+    if no is None:
+      no = self.raiz
+
+    altura_esquerda = 0
+    altura_direita = 0
+
+    if(no.esquerda):
+      altura_esquerda = self.altura(no.esquerda)
+    if(no.direita):
+      altura_direita = self.altura(no.direita)
+    if(altura_direita > altura_esquerda):
+      return altura_direita + 1
+    return altura_esquerda + 1
+
+  def getProfundidadeNo():
+    pass
+  
+  def getNivel():
+    pass
--- main.py
+++ main.py
@@ -1,28 +1,34 @@
 from ArvoreBinaria import ArvoreBinaria
-from No import No
 
-arvore = ArvoreBinaria()
+if __name__ == '__main__':
+  arvore = ArvoreBinaria()
 
-arvore.inserir(53)
-arvore.inserir(30)
-arvore.inserir(14)
-arvore.inserir(39)
-arvore.inserir(9)
-arvore.inserir(23)
-arvore.inserir(34)
-arvore.inserir(49)
-arvore.inserir(72)
-arvore.inserir(61)
-arvore.inserir(84)
-arvore.inserir(79)
-arvore.inserir(89)
+  arvore.inserir(53)
+  arvore.inserir(30)
+  arvore.inserir(14)
+  arvore.inserir(39)
+  arvore.inserir(9)
+  arvore.inserir(23)
+  arvore.inserir(34)
+  arvore.inserir(49)
+  arvore.inserir(72)
+  arvore.inserir(61)
+  arvore.inserir(84)
+  arvore.inserir(79)
+  arvore.inserir(89)
 
-print(arvore.raiz.esquerda.valor)
-print(arvore.raiz.direita.valor)
-print(arvore.ligacoes)
+  print()
+  print(arvore.ligacoes)
+  print()
+  print(f'Nó raiz principal: {arvore.raiz.valor}')
+  print(f'Nó raiz esquerdo: {arvore.raiz.esquerda.valor}')
+  print(f'Nó raiz direito: {arvore.raiz.direita.valor}')
+  print()
+  print('#' * 25)
+  print(f'Altura: {arvore.altura()}')
 
-arvore.pesquisar(89)
-if arvore.pesquisar(89) is None:
-  print('Elemento não localizado.')
-else:
-  print('Elemento localizado.')
+  # arvore.pesquisar(89)
+  # if arvore.pesquisar(89) is None:
+  #   print('Elemento não localizado.')
+  # else:
+  #   print('Elemento localizado.')
